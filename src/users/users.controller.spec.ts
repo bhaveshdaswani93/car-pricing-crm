f972b75f5bbec5ea96ddeb6a7e747a2343af77e1
bhavesh <bhaveshdaswani93@gmail.com>
2022-04-01 15:42:38 +0530

    Created unit test for user controller.

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -10,7 +10,10 @@ describe('UsersController', () => {
   let authService: Partial<AuthService>;
 
   beforeEach(async () => {
-    usersService = {};
+    usersService = {
+      findOne: (id: number) =>
+        Promise.resolve({ id, email: 'abc@a.com', password: '123' } as User),
+    };
 
     authService = {
       signup: (email, password) =>
@@ -40,10 +43,16 @@ describe('UsersController', () => {
   it('signup a user', async () => {
     const email = 'a@a.com';
     const password = 'asdf';
-    const session = {};
+    const session = {} as { [key: string]: any };
     const user = await controller.signup({ email, password }, session);
     expect(user).toBeDefined();
     expect(user.email).toEqual(email);
     expect(user.id).toBeDefined();
+    expect(session.userid).toEqual(1);
+  });
+
+  it('get the individual user by provided id', async () => {
+    const user = await controller.getUser('1');
+    expect(user.id).toEqual(1);
   });
 });
